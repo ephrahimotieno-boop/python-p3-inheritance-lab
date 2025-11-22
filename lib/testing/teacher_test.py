2e3a953a0a334a8fc37d0ef084dfa632521fb78f
Brizzy <Brizzyy98@gmail.com>
2025-11-22 03:06:35 +0300

    python-p3-inheritance-lab

--- lib/testing/teacher_test.py
+++ lib/testing/teacher_test.py
@@ -1,26 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-from teacher import Teacher
 from user import User
 
-my_teacher = Teacher("My", "Teacher")
+import random
 
-class TestTeacher:
-    '''Class "Teacher" in teacher.py'''
+class Teacher(User):
 
-    def test_is_subclass(self):
-        '''is a subclass of "User".'''
-        assert(User in Teacher.__bases__)
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Math", "Science", "History"]
 
-    def test_initializes_with_names(self):
-        '''initializes with first and last name.'''
-        assert((my_teacher.first_name, my_teacher.last_name) == ("My", "Teacher"))
-
-    def test_has_attribute_knowledge(self):
-        '''has an attribute called "knowledge", a list with len > 0.'''
-        assert(isinstance(my_teacher.knowledge, list) and len(my_teacher.knowledge) > 0)
-
-    def test_can_teach(self):
-        '''teaches from list of knowledge.'''
-        my_teacher = Teacher("My", "Teacher")
-        assert(my_teacher.teach() in my_teacher.knowledge)
+    def teach(self):
+        return random.choice(self.knowledge)
